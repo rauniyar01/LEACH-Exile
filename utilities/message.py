6d90e0c216c96b61734533ef25f6db6500e497a7
nmpiazza <nmp5860@rit.edu>
2017-04-27 13:32:06 -0400

    Comment out crypto related code and references to the signature

--- utilities/message.py
+++ utilities/message.py
@@ -1,8 +1,5 @@
 import json
-#import hashlib
 import base64
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazman.primitives.asymmetric import padding
 
 #TODO: Create Messsage class
 class Message(object):
@@ -11,7 +8,7 @@ class Message(object):
         self.id_str    = id_str   #ID string of the node
         self.dst       = dst      #IP:port address of the destination
         self.data      = data     #Data of the message being sent
-        self.signature = self.sign()
+        #self.signature = self.sign()
 
     #TODO: Convert Message object to JSON blob
     def to_json(self):
@@ -26,52 +23,52 @@ class Message(object):
         self.id_str    = msg[0]
         self.dst_addr  = msg[1]
         self.data      = msg[2]
-        self.signature = msg[3]
+        #self.signature = msg[3]
 
-    #TODO: Sign message
-    def sign(self, key):
-        """Key will be passed in by the caller.
-        This is determined by whether the node is a node or the sink"""
+    ##TODO: Sign message
+    #def sign(self, key):
+    #    """Key will be passed in by the caller.
+    #    This is determined by whether the node is a node or the sink"""
 
-        #Create a signer with SHA512
-        signer = key.signer(
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA512()),
-                salt_length=padding.PSS.MAX_LENGTH
-            ),
-            hashes.SHA512()
-        )
+    #    #Create a signer with SHA512
+    #    signer = key.signer(
+    #        padding.PSS(
+    #            mgf=padding.MGF1(hashes.SHA512()),
+    #            salt_length=padding.PSS.MAX_LENGTH
+    #        ),
+    #        hashes.SHA512()
+    #    )
 
-        signer.update(self.id_str)
-        signer.update(self.dst)
-        signer.update(self.data)
-        self.signature = m.finalize()
+    #    signer.update(self.id_str)
+    #    signer.update(self.dst)
+    #    signer.update(self.data)
+    #    self.signature = m.finalize()
 
-    #TODO: Verify message with crypto
-    def verify(self, key):
-        """Returns boolean of verification status"""
-        verifier = key.verify(
-            self.signature,
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH
-            ),
-        )
-        verifier.update(self.id_str)
-        verifier.update(self.dst)
-        verifier.update(self.data)
+    ##TODO: Verify message with crypto
+    #def verify(self, key):
+    #    """Returns boolean of verification status"""
+    #    verifier = key.verify(
+    #        self.signature,
+    #        padding.PSS(
+    #            mgf=padding.MGF1(hashes.SHA256()),
+    #            salt_length=padding.PSS.MAX_LENGTH
+    #        ),
+    #    )
+    #    verifier.update(self.id_str)
+    #    verifier.update(self.dst)
+    #    verifier.update(self.data)
 
-        return verifier.verify()
+    #    return verifier.verify()
 
-    #TODO: Sign and send message if the node is a sink
-    def sign_and_send(self, key):
-        """Key will be passed in by the caller.
-        This is determined by whether the node is a node or the sink"""
-        pass
+    ##TODO: Sign and send message if the node is a sink
+    #def sign_and_send(self, key):
+    #    """Key will be passed in by the caller.
+    #    This is determined by whether the node is a node or the sink"""
+    #    pass
 
 def message_decoder(obj):
     if '__type__' in obj and obj['__type__'] == 'Message':
-        return (obj['id_str'], obj['dst_addr'], obj['data'], obj['signature'])
+        return (obj['id_str'], obj['dst_addr'], obj['data'])#, obj['signature'])
 
 
 #TODO: Send_message function
