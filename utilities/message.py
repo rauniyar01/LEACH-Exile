188c0d1db7f6bb3be73855f5dfdd25c757856729
nmpiazza <nmp5860@rit.edu>
2017-04-30 13:10:06 -0400

    Prepare to change from class to set of functions. Remove commented code and message class.

--- utilities/message.py
+++ utilities/message.py
@@ -3,79 +3,29 @@ import base64
 from hashlib import md5
 from time import time
 
-# TODO: Create Messsage class
-class Message(object):
+#TODO: Send_message function
+def send_message():
+    """You provide it data and then it will marshall the JSON and send it
+    Tell it the destination, which is previously determined through the election process"""
+    pass
+##TODO: Change this into a standalone function
+def to_json(self):
+    return json.dumps(self, default=lambda o: o.__dict__,
+            sort_keys=True, indent=4)
 
-    def __init__(self, id_str=None, dst=None, data=None):
-        # Time is sufficient for entropy since it is just needs to be unique, not crypto related
-        if not id_str:
-            self.id_str = md5().hexdigest(time())
-        else:
-            self.id_str = id_str
+##TODO: Change this into a standalone function
+def from_json(self, json_blob):
+    """Call the decoder function, which returns a quadruple.
+    Transfer the data from the quadruple into the fields of the Message"""
+    msg = Message()
+    msg.__dict__  = json.loads(json_blob)#, object_hook=message_decoder)
+    self.id_str   = msg[0]
+    self.dst_addr = msg[1]
+    self.data     = msg[2]
+# self.signature = msg[3]
+    return msg
 
-        self.dst  = dst      # IP:port address of the destination
-        self.data = data     # Data of the message being sent
-        # self.signature = self.sign()
-
-    # TODO: Convert Message object to JSON blob
-    def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__,
-                sort_keys=True, indent=4)
-
-    # TODO: Parse JSON blob into Message object
-    def from_json(self, json_blob):
-        """Call the decoder function, which returns a quadruple.
-        Transfer the data from the quadruple into the fields of the Message"""
-        msg = Message()
-        msg.__dict__  = json.loads(json_blob)#, object_hook=message_decoder)
-        self.id_str   = msg[0]
-        self.dst_addr = msg[1]
-        self.data     = msg[2]
-    # self.signature = msg[3]
-        return msg
-
-    #TODO: Sign message
-    #def sign(self, key):
-    #    """Key will be passed in by the caller.
-    #    This is determined by whether the node is a node or the sink"""
-
-    #    #Create a signer with SHA512
-    #    signer = key.signer(
-    #        padding.PSS(
-    #            mgf=padding.MGF1(hashes.SHA512()),
-    #            salt_length=padding.PSS.MAX_LENGTH
-    #        ),
-    #        hashes.SHA512()
-    #    )
-
-    #    signer.update(self.id_str)
-    #    signer.update(self.dst)
-    #    signer.update(self.data)
-    #    self.signature = m.finalize()
-
-    ##TODO: Verify message with crypto
-    #def verify(self, key):
-    #    """Returns boolean of verification status"""
-    #    verifier = key.verify(
-    #        self.signature,
-    #        padding.PSS(
-    #            mgf=padding.MGF1(hashes.SHA256()),
-    #            salt_length=padding.PSS.MAX_LENGTH
-    #        ),
-    #    )
-    #    verifier.update(self.id_str)
-    #    verifier.update(self.dst)
-    #    verifier.update(self.data)
-
-    #    return verifier.verify()
-
-    ##TODO: Sign and send message if the node is a sink
-    #def sign_and_send(self, key):
-    #    """Key will be passed in by the caller.
-    #    This is determined by whether the node is a node or the sink"""
-    #    pass
-
-
+#NOTE: Might not need this anymore
 def message_decoder(obj):
     print obj
     print type(obj)
@@ -84,9 +34,3 @@ def message_decoder(obj):
         return (obj['id_str'], obj['dst_addr'], obj['data'])#, obj['signature'])
 
 
-#TODO: Send_message function
-# Probably shouldn't be a method of the Message class
-def send_message():
-    """You provide it data and then it will marshall the JSON and send it
-    Tell it the destination, which is previously determined through the election process"""
-    pass
