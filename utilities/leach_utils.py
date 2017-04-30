df4b266a7fe8f11dd15d485f1ad1ecb74fee06ff
nmpiazza <nmp5860@rit.edu>
2017-04-30 15:15:32 -0400

    Consolidate message.py and leach_utils.py

--- utilities/leach_utils.py
+++ utilities/leach_utils.py
@@ -1,6 +1,12 @@
 import json
 from message import *
 
+#TODO: Create constant for valid data
+VALID_JSON = "{'id_str': '098f6bcd4621d373cade4e832627b4f6', 'dest': '127.0.0.1:50000', 'data': 'data'}"
+
+#TODO: Create constant for malicious data
+INVALID_JSON ="{'id_str': '098f6bcd4621d373cade4e832627b4f6', 'dest': '127.0.0.1:50000', 'data': 'data'}"
+
 exiled = []
 
 #Check if node is exiled
@@ -10,7 +16,11 @@ def is_exiled(id_str):
 #Receive message and forward it to the sink
 def recv_and_fwd(sock, id_str, upstream_addr):
     recvd = recv_message(sock)
-    send_message(sock, id_str, upstream_addr, json.dumps(recvd))
+
+    #Forward the message only if the message did not come from the sender
+    if recvd.id_str != upstream_addr:
+        send_message(sock, id_str, upstream_addr, json.dumps(recvd))
+    return recvd
 
 def exile(node):
     global exiled
@@ -25,3 +35,27 @@ def welcome(node):
 def exiled_list():
     global exiled
     return exiled
+
+#Take a socket object, create a json blob, and send it
+def send_message(sock, id_str, dest, data):
+    
+    #Build the json from the supplied data
+    data = json.dumps({'id_str': id_str, 'dest': dest, 'data': data})
+
+    #make a connection and send it to the destination
+    try:
+        sock.connect(dest)
+        sock.send(data)
+        sock.close()
+    except socket.error as e:
+        print "Could not send message: {}".format(e)
+        return False
+
+    return True
+
+#Receive message and return a dictionary
+#This allows you to do forward the entire message later
+def recv_message(socket):
+    data = sock.recv()
+    return json.loads(data)
+
