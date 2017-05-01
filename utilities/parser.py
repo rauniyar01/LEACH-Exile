b5d6a2453189fd08432284e68499641a7fd29460
Bojak <jpb4616@rit.edu>
2017-05-01 16:29:17 -0400

    Made examples much simplier

--- utilities/parser.py
+++ utilities/parser.py
@@ -3,31 +3,31 @@ import json
 # Example JSON objects for each command #
 #########################################
 
-VALID_EXILE = "{\"id_str\": \"127.0.0.1:50001\", \"data\": { \
-\"cmd\": \"exile\" \
-\"data\":\"127.0.0.1:50002\" \
+VALID_EXILE = '{"id_str": "127.0.0.1:50001", "data": { \
+"cmd": "exile" \
+"data":"127.0.0.1:50002" \
 } \
-}"
+}'
 
-VALID_WELCOME = "{\"id_str\": \"127.0.0.1:50001\", \"data\": { \
-\"cmd\": \"welcome\" \
-\"data\":\"127.0.0.1:50002\" \
+VALID_WELCOME = '{"id_str": "127.0.0.1:50001", "data": { \
+"cmd": "welcome" \
+"data":"127.0.0.1:50002" \
 } \
-}"
+}'
 
-VALID_DATA = "{\"id_str\": \"127.0.0.1:50001\", \"data\": { \
-\"cmd\": \"data\" \
-\"data\":\"data\" \
+VALID_DATA = '{"id_str": "127.0.0.1:50001", "data": { \
+"cmd": "data" \
+"data":"data" \
 } \
-}"
+}'
 
 #50003 is our CH node in this example
-VALID_FORWARD = "{\"id_str\": \"127.0.0.1:50003\", \"data\": { \
-\"cmd\": \"forward\", \
-\"orig_source\": \"127.0.0.1:50001\", \
-\"data\":\"data\" \
+VALID_FORWARD = '{"id_str": "127.0.0.1:50003", "data": { \
+"cmd": "forward", \
+"orig_source": "127.0.0.1:50001", \
+"data":"data" \
 }\
-}"
+}'
 
 #TODO: Parse all of the json data
 #Takes in a JSON string, decodes it, and then returns the resultant JSON
@@ -40,7 +40,7 @@ def json_to_str(data):
 #Takes all of the data required to built the JSON objects
 #orig_source is only populated if the command is "forward"
 #L2_data is 
-def vals_to_json(id_str, cmd, orig_source=None, L2_data):
+def vals_to_json(id_str, cmd, L2_data, orig_source=None):
     #build the json from the provided information
     #have separate cases for each command type
 
@@ -48,7 +48,7 @@ def vals_to_json(id_str, cmd, orig_source=None, L2_data):
     pass
 
 ##TODO: Encode Layer one data 
-#def enccode_layer_one():
+#def encode_layer_one():
 #    pass
 #
 ##TODO: Encode layer 2 data
